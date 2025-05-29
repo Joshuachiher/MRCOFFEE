4735f63edb32438c230d3a3322bd3e98e0510413
joshuachiher <Joshuachiher@gmail.com>
2025-05-29 21:21:26 +0700

    Login Method

---
+++ src/config/firebase.jsx
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBNrjhKgV3PczmYIgOlUqm334YgqsJzawo",
+  authDomain: "mrcoffee-61d1d.firebaseapp.com",
+  projectId: "mrcoffee-61d1d",
+  storageBucket: "mrcoffee-61d1d.firebasestorage.app",
+  messagingSenderId: "1004878923759",
+  appId: "1:1004878923759:web:46d26de8e340e05263981a",
+  measurementId: "G-HZWMDPWQPK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app)
